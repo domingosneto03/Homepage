e3392350d85f4dd76a71b7af56ccbcd1e794e848
Inês Oliveira <up202103343@up.pt>
2022-10-19 23:32:00 +0100

    imprimir horarios das turmas mas o map nao funciona

--- Application.cpp
+++ Application.cpp
@@ -9,6 +9,32 @@
 
 using namespace std;
 
+map <string, string> UcsMap = {{"L.EIC001", "ALGA"},
+                               {"L.EIC002", "AM I"},
+                               {"L.EIC003", "FP"},
+                               {"L.EIC004", "FSC"},
+                               {"L.EIC005", "MD"},
+                               {"L.EIC006", "AC"},
+                               {"L.EIC007", "AM II"},
+                               {"L.EIC008", "F I"},
+                               {"L.EIC009", "P"},
+                               {"L.EIC010", "TC"},
+                               {"L.EIC011", "AED"},
+                               {"L.EIC012", "BD"},
+                               {"L.EIC013", "F II"},
+                               {"L.EIC014", "LDTS"},
+                               {"L.EIC015", "SO"},
+                               {"L.EIC016", "DA"},
+                               {"L.EIC017", "ES"},
+                               {"L.EIC018", "LC"},
+                               {"L.EIC019", "LTW"},
+                               {"L.EIC020", "ME"},
+                               {"L.EIC021", "FSI"},
+                               {"L.EIC022", "IPC"},
+                               {"L.EIC023", "LBAW"},
+                               {"L.EIC024", "PFL"},
+                               {"L.EIC025", "RC"}};
+
 void Application() {
 
 }
@@ -95,9 +121,34 @@ vector<Student> Application::readStudents() {
             n++;
         }
     }
-    int teste = 0;
-    teste++;
     return list;
+}
+
+vector<schedule> Application::readClassesPerStudent() {
+    vector<schedule> classesPerStudent = {};
+    fstream fin;
+    fin.open("./schedule/classes_per_uc.csv", ios::in);
+    vector<string> row;
+    string line, word, temp;
+    int count = 0;
+    int n = 0;
+    while (true) {
+        row.clear();
+        getline(fin, line);
+        if (line == "") break;
+        stringstream s(line);
+        while (getline(s, word, ',')) {
+            row.push_back(word);
+        }
+        if (count == 0) {
+            count++;
+            continue;
+        }
+        string UcCode = row[0];
+        string ClassCode = row[1];
+        classesPerStudent.push_back({ClassCode, UcCode});
+    }
+    return classesPerStudent;
 }
 
 //neste metodo criamos uma lista que junta tudo do primeiro e ultimo ficheiros
@@ -111,7 +162,8 @@ vector<studentAndClass> Application::StudentClass() {
             vector<string> st_class = st.getClass();
             if (st_class[1] == uniClassList[i].getClassCode() && st_class[0] == uniClassList[i].getUcCode()) {
                 studentAndClasses.push_back(
-                        {st.getStudentCode(), st.getName(), uniClassList[i].getClassCode(), uniClassList[i].getUcCode(), uniClassList[i].getWeekDay(),
+                        {st.getStudentCode(), st.getName(), uniClassList[i].getClassCode(), uniClassList[i].getUcCode(),
+                         uniClassList[i].getWeekDay(),
                          uniClassList[i].getClassType(), uniClassList[i].getDuration()});
             }
         }
@@ -119,13 +171,27 @@ vector<studentAndClass> Application::StudentClass() {
     return studentAndClasses;
 }
 
+//neste metodo criamos uma lista com os estudantes que tinham o up que o utilizador introduziu
 vector<studentAndClass> Application::StudentSchedule(string studentCode) {
     vector<studentAndClass> studentAndClasses = StudentClass();
     vector<studentAndClass> studentSchedule = {};
-    for (int i = 0; i < studentAndClasses.size(); ++i) {
+    for (int i = 0; i < studentAndClasses.size(); i++) {
         if (studentAndClasses[i].studentCode == studentCode) {
             studentSchedule.push_back(studentAndClasses[i]);
         }
     }
     return studentSchedule;
+}
+
+
+vector <schedule> Application::ClassesSchedule(string classCode) {
+    vector<schedule> classesPerStudentList = readClassesPerStudent();
+    vector<schedule> classSchedule = {};
+
+    for (int i = 0; i < classesPerStudentList.size(); i++) {
+        if (classesPerStudentList[i].classCode == classCode) {
+            classSchedule.push_back(classesPerStudentList[i]);
+        }
+    }
+    return classSchedule;
 }
--- Menu.cpp
+++ Menu.cpp
@@ -18,6 +18,7 @@ void Menu::mainMenu() {
     int option;
     cout << "=======================================" << endl;
     cout << "1 - Horario de estudante" << endl;
+    cout << "2 - Horario de uma turma" << endl;
     cout << "2 - Menu das turmas" << endl;
     cout << "3 - Sair" << endl;
     cout << "=======================================" << endl;
@@ -32,7 +33,7 @@ void Menu::mainMenu() {
             SchedulePerStudent();
             break;
         case 2:
-            TurmasMenu();
+            SchedulePerClass();
             break;
         case 3:
             break;
@@ -51,17 +52,42 @@ void Menu::SchedulePerStudent() {
             break;
         }
     }
-
     vector<string> weekdayNames = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
     vector<string> classTypeNames = {"T", "TP", "PL"};
-    cout << "Horario da " << nome << ", com up" << option << endl;
+    cout << "Horario de " << nome << ", up" << option << endl;
     for (int i = 0; i < studentAndClass.size(); i++) {
         cout << "WeekDay: " << weekdayNames[studentAndClass[i].weekDay] << " | UcCode: " << studentAndClass[i].ucCode << " | ClassCode: " << studentAndClass[i].classCode << " | ClassType: " << classTypeNames[studentAndClass[i].classType] << " | Duration: " << studentAndClass[i].duration << endl;
     }
 }
 
-void Menu::TurmasMenu() {
+void Menu::SchedulePerClass() {
+    int ano;
+    int turma;
+    string turmaFinal;
+    cout << "Introduza o ano curricular (1 a 3): "  << endl;
+    cin >> ano;
+    while(ano < 1 || ano > 3) {
+        cout << "O ano introduzido e invalido! Introduza novamente: "  << endl;
+        cin >> ano;
+    }
+    cout << "Introduza o numero da turma (1 a 16): "  << endl;
+    cin >> turma;
+    while(turma < 1 || turma > 16) {
+        cout << "A turma introduzida e invalida! Introduza novamente: "  << endl;
+        cin >> turma;
+    }
+    if (turma > 0 && turma < 9) {
+        turmaFinal = to_string(ano) + "LEIC0" + to_string(turma);
+    }
+    else {
+        turmaFinal = to_string(ano) + "LEIC" + to_string(turma);
+    }
 
+    vector <schedule> classesSchedule = app.ClassesSchedule(turmaFinal);
+    cout << "A turma " << turmaFinal << " tem o seguinte horario:" << endl;
+    for (int i = 0; i < classesSchedule.size(); i++) {
+        cout << "ClassCode: " << classesSchedule[i].classCode << " | UcCode: " << classesSchedule[i].ucCode << endl; // << " | UC: " << UcsMap[classesSchedule[i].ucCode];
+    }
 }
 
 #endif // PROJECT_AED_MENU_CPP
